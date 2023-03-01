669630eb589f99212bf49af7cf872c941b54f4e9
squach28 <squach3443@gmail.com>
2023-03-01 08:49:46 -0800

    fixed accordion not retracting

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ console.log(listElements)
 
 listElements.forEach(element => {
     element.addEventListener('click', (e) => {
-        deselectTopicHeaders()
-        element.classList.add('topic-header-active')
         const arrowElement = element.children[1]
         const detailsElement = element.nextElementSibling
         if(detailsElement.classList.contains('hidden')) {
+            deselectTopicHeaders()
+            element.classList.add('topic-header-active')
             detailsElement.classList.remove('hidden')
             detailsElement.classList.add('active')
             arrowElement.classList.add('arrow-up')
         } else {
+            deselectTopicHeaders()
+            element.classList.add('topic-header-active')
             detailsElement.classList.remove('active')
             detailsElement.classList.add('hidden')
             arrowElement.classList.remove('arrow-up')
